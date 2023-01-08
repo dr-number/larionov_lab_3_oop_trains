69ab46a2ab48113323e2a26c7ffc09dd2bdeca37
Lar97 <dr.number@yandex.ru>
2023-01-09 00:24:50 +0700

    correcting

--- larionov_lab_3_oop_trains/Program.cs
+++ larionov_lab_3_oop_trains/Program.cs
@@ -6,17 +6,17 @@
 
         private const int MIN_COUNT_TRAIN = 3;
         private const int MAX_COUNT_TRAIN = 10;
+        private const string FILE_TRAINS = "file_trains.txt";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Варинат №16. Железнодорожный вокзал. Ларионов Никита Юрьевич. гр. 210з\n");
             MyInput myInput = new MyInput();
 
-            bool isGo = true;
             string select;
             Stantion stantion;
 
-            while (isGo)
+            while (true)
             {
                 Console.ResetColor();
                 Console.WriteLine($"\nЧтобы выйти ввидите {EXIT}");
@@ -25,22 +25,20 @@
                 stantion = new Stantion();
 
                 if (select == EXIT)
-                    isGo = false;
+                    break;
+                
+                if (MyMessages.isQuestion(Constants.QUESTION_LOAD_FROM_FILE))
+                    stantion.loadFromFile(FILE_TRAINS);
                 else
                 {
-                    if (MyMessages.isQuestion(Constants.QUESTION_LOAD_FROM_FILE))
-                    {
+                    int countTrains = myInput.inputInterval("Сколько поездов добавить на станцию?: ", MIN_COUNT_TRAIN, MAX_COUNT_TRAIN);
 
-                    }
-                    else
-                    {
-                        int countTrains = myInput.inputInterval("Сколько поездов добавить на станцию?: ", MIN_COUNT_TRAIN, MAX_COUNT_TRAIN);
+                    for (int i = 0; i < countTrains; i++)
+                        stantion.addTrain();
 
-                        for (int i = 0; i < countTrains; i++)
-                            stantion.addTrain();
-
-                    }
                 }
+
+
 
             }
 
--- larionov_lab_3_oop_trains/Stantion.cs
+++ larionov_lab_3_oop_trains/Stantion.cs
@@ -16,7 +16,55 @@
             stantion.addTrain(train);
         }
 
-        private List<ModelTrain> loadFromFile(string kFileName)
+        public void printTrains(List<ModelTrain> data)
+        {
+
+        }
+
+        public bool saveToFile(string fileName, List<ModelTrain> data)
+        {
+            StreamWriter file = null;
+            bool result = false;
+            try
+            {
+                string departureTime;
+                file = new StreamWriter(fileName);
+                foreach (var item in data)
+                {
+                    departureTime = $"{item.DepartureTime.getHour()}:{item.DepartureTime.getMinute()}";
+                    file.WriteLine($"{item.Number}, {item.Destination}, {departureTime}");
+                }
+
+                file.Close();
+                result = true;
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка при записи в файл: {e.Message}");
+                result = false;
+            }
+            finally
+            {
+                try
+                {
+                    file.Close();
+                }
+                catch (Exception ignore) { }
+            }
+
+            if (result)
+                Console.WriteLine($"\nДанные успешно сохранены в файл: {fileName}!");
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\nОшибка при сохранении данных в файл: {fileName}!");
+            }
+
+            return result;
+        }
+
+        public List<ModelTrain> loadFromFile(string kFileName)
         {
             if (!File.Exists(kFileName))
             {
