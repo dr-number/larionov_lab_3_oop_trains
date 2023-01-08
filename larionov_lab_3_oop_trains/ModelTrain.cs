ff99a95ee07a22b7c6ed3be536995a1cb9666dd8
Lar97 <dr.number@yandex.ru>
2023-01-09 01:03:46 +0700

    overload operators

--- larionov_lab_3_oop_trains/ModelTrain.cs
+++ larionov_lab_3_oop_trains/ModelTrain.cs
@@ -28,5 +28,35 @@
             return $"{departureTime.getHour()}:{departureTime.getMinute()}";
         }
 
+        public static bool operator >(ModelTrain t1, ModelTrain t2)
+        {
+            return t1.DepartureTime > t2.DepartureTime;
+        }
+        public static bool operator <(ModelTrain t1, ModelTrain t2)
+        {
+            return t1.DepartureTime < t2.DepartureTime;
+        }
+        public static bool operator ==(ModelTrain t1, ModelTrain t2)
+        {
+            return t1.DepartureTime == t2.DepartureTime;
+        }
+        public static bool operator !=(ModelTrain t1, ModelTrain t2)
+        {
+            return t1.DepartureTime != t2.DepartureTime;
+        }
+
+        public int CompareTo(ModelTrain train)
+        {
+
+
+            if (departureTime > train.departureTime)
+                return 1;
+            else if (departureTime < train.departureTime)
+                return -1;
+            else
+                return 0;
+
+        }
+
     }
 }
--- larionov_lab_3_oop_trains/MyTime.cs
+++ larionov_lab_3_oop_trains/MyTime.cs
@@ -5,11 +5,37 @@ namespace larionov_lab_3_oop_trains
     {
         private int hour;
         public void setHour(int hour) { this.hour = hour; }
-        public int getHour() { return this.hour; }
+        public int getHour() { return hour; }
 
         public int minute;
         public void setMinute(int minute) { this.minute = minute; }
-        public int getMinute() { return this.minute; }
+        public int getMinute() { return minute; }
+
+
+        public static bool operator >(MyTime t1, MyTime t2)
+        {
+            int time1 = t1.hour * 60 + t1.minute;
+            int time2 = t2.hour * 60 + t2.minute;
+            return time1 > time2;
+        }
+        public static bool operator <(MyTime t1, MyTime t2)
+        {
+            int time1 = t1.hour * 60 + t1.minute;
+            int time2 = t2.hour * 60 + t2.minute;
+            return time1 < time2;
+        }
+        public static bool operator ==(MyTime t1, MyTime t2)
+        {
+            int time1 = t1.hour * 60 + t1.minute;
+            int time2 = t2.hour * 60 + t2.minute;
+            return time1 == time2;
+        }
+        public static bool operator !=(MyTime t1, MyTime t2)
+        {
+            int time1 = t1.hour * 60 + t1.minute;
+            int time2 = t2.hour * 60 + t2.minute;
+            return time1 != time2;
+        }
 
     }
 }
